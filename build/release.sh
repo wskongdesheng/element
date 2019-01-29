8e215a6535d65bfbe3ebeb90277128315e38693a
wskongdesheng <wskongdesheng@163.com>
2019-01-29 13:59:46 +0800

    up

--- build/release.sh
+++ build/release.sh
@@ -27,17 +27,8 @@ then
   fi
   cd ../..
 
-  # commit
-  git add -A
-  git commit -m "[build] $VERSION"
   npm version $VERSION --message "[release] $VERSION"
 
-  # publish
-  git push eleme master
-  git push eleme refs/tags/v$VERSION
-  git checkout dev
-  git rebase master
-  git push eleme dev
 
   if [[ $VERSION =~ "beta" ]]
   then
